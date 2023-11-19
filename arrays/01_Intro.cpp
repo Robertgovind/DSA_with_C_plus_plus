695017dab74d3d8f41339019bb723701618ffcbc
Govind Kumar Yadav <robertgovind@gmail.com>
2023-11-19 09:17:09 +0545

    second day

---
+++ arrays/01_Intro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // for loop
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // for each loop
+    for (int arr : arr)
+    {
+        cout << arr << " ";
+    }
+    return 0;
+}
---
+++ arrays/02_traversing.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int arr[n];
+    cin >> n;
+    cout << n;
+    return 0;
+}
