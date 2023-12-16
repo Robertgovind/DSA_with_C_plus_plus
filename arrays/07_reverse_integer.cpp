d64343eaed5118cdbd63dcf359065d037ff1b2e7
Govind Kumar Yadav <robertgovind@gmail.com>
2023-12-16 19:58:24 +0545

    modified arrays

--- arrays/07_reverse_integer.cpp
+++ arrays/07_reverse_integer.cpp
@@ -5,6 +5,8 @@ int reverseInteger(int x){
     int ans=0;
     while(x!=0){
         int digit=x%10;
+
+        //Used for overflow and underflow condition of an integer
         if(ans>INT32_MAX/10||ans<INT32_MIN/10){
             return 0;
         }
@@ -18,5 +20,5 @@ int main(){
     int number = 123456789;
     int ans=reverseInteger(number);
     cout<<"The reversed number is : "<<ans<<endl;
-    
+
 }
