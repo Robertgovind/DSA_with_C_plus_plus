e530fce060d89560504a5388fb345442550c5a1b
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-02 21:47:06 +0545

    added leetcode problem

--- 03_arrays/16_merge_sorted_array.cpp
+++ 03_arrays/16_merge_sorted_array.cpp
@@ -28,6 +28,26 @@ void mergeSortedArray(int arr1[],int n,int arr2[],int m,int arr3[]){
     }
 }
 
+
+//This code works for leetcode but gets runtime error
+void insertElement(int arr1[],int pos,int n,int ele){
+    for(int i=n-1;i>pos;i--){
+        arr1[i]=arr1[i-1];
+    }
+    arr1[pos]=ele;
+}
+void mergeSortedArray1(int arr1[],int n,int arr2[],int m){
+    int i=0,j=0;
+    while(j<m){
+        if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else if((arr1[i]==0)|| arr1[i]>=arr2[j]){
+            insertElement(arr1,i,n,arr2[j++]);
+        }
+    }
+}
+
 void printArray(int arr[],int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
@@ -36,9 +56,9 @@ void printArray(int arr[],int n){
 }
 
 int main(){
-    int arr1[5]={1,3,5,7,9};
+    int arr1[8]={1,3,5,7,9,0,0,0};
     int arr2[3]={2,4,6};
     int arr3[8]={0};
-    mergeSortedArray(arr1,5,arr2,3,arr3);
-    printArray(arr3,8);
+    mergeSortedArray1(arr1,8,arr2,3);
+    printArray(arr1,8);
 }
--- 03_arrays/22_intersection_of_two_array.cpp
+++ 03_arrays/22_intersection_of_two_array.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 vector<int> intersection(vector<int> &v1,vector<int> &v2){
     int n=v1.size(),m=v2.size();
     vector<int> ans;
     int i=0,j=0;
-    int prev;
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
+    int prev=-1;
     while (i<n&&j<m)    
     {
         if(v1[i]==v2[j]){
+            if(prev==v1[i]){
+                i++;
+                j++;
+                continue;
+            }
+            prev=v1[i];
             ans.push_back(v1[i++]);
             j++;
         }
@@ -24,16 +33,16 @@ vector<int> intersection(vector<int> &v1,vector<int> &v2){
 
 int main(){
     vector<int> v1;
-    v1.push_back(1);
-    v1.push_back(2);
-    v1.push_back(2);
-    v1.push_back(1);
-    //v1.push_back(6);
+    v1.push_back(9);
+    v1.push_back(4);
+    v1.push_back(9);
+    v1.push_back(8);
+    v1.push_back(4);
 
     vector<int> v2;
-    v2.push_back(2);
-    v2.push_back(2);
-    //v2.push_back(6);
+    v2.push_back(4);
+    v2.push_back(9);
+    v2.push_back(5);
     //v2.push_back(7);
     
     vector<int> ans=intersection(v1,v2);
