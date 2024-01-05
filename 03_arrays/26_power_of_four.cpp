9466592b25f54d3976894042f36fc669dac2100d
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-05 22:46:39 +0545

    leetcode problem

---
+++ 03_arrays/26_power_of_four.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+bool checkPowerOfFour(int n){
+    while(n!=0){
+        if(n==1)
+        return true;
+        if((n%4)!=0)
+        return false;
+
+        n=n>>2;
+    }
+    return true;
+}
+
+int main(){
+    cout<<checkPowerOfFour(256);
+    
+    return 0;
+}
