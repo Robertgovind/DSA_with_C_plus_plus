6fd32ccc3c5281eb75201462afb5e9ae1b8e1158
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-12 23:02:32 +0545

    Working with pointer

---
+++ 09_pointers/01_intro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   cout<<"Pointers :"<<endl;
+   int a=5;
+   cout<<a<<endl;
+   int *ptr=&a;
+   cout<<&a<<endl;
+   cout<<ptr<<endl;
+   cout<<*ptr<<endl;
+    return 0;
+}
