900cfd08ba82846ebcf5d682b589d1fc2a1ebead
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-24 00:09:16 +0545

    insert program

--- 13_Linked_List.cpp/01_singly_linked_list.cpp
+++ 13_Linked_List.cpp/01_singly_linked_list.cpp
@@ -11,6 +11,7 @@ class Node{
         this->next = NULL;
     }
 };
+
 void insertAtHead(Node* &head,int data){
     Node* NewNode = new Node(data);
 
@@ -18,9 +19,35 @@ void insertAtHead(Node* &head,int data){
     head = NewNode;
 }
 
+void insertAtTail(Node* &tail,int data){
+    Node* newNode=new Node(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void insertAtPos(Node* &head,Node* &tail, int pos,int data){
+    if(pos == 1){
+        insertAtHead(head,data);
+        return ;
+    }
+    Node* temp = head;
+    int count =1;
+    while(count<pos - 1){
+        temp=temp->next;
+        count++;
+    }
+    if(temp == NULL){
+        insertAtTail(tail,data);
+        return; 
+    }
+    Node* newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
 void print(Node* head){
     Node* temp = head;
-    while(head != NULL){
+    while(temp != NULL){
         cout<<temp -> data << " ";
         temp = temp -> next;
     }
@@ -28,12 +55,23 @@ void print(Node* head){
 }
 
 int main(){
-    Node* head = NULL;
-    Node* tail = NULL;
+    Node* head = new Node(5);
+    Node* tail = head;
 
-    insertAtHead(head,12);
+    insertAtHead(head,44);
     print(head);
-    insertAtHead(head, 15);
+
+    insertAtTail(tail,12);
     print(head);
+    insertAtTail(tail, 15);
+    print(head);
+    insertAtTail(tail,22);
+    print(head);
+
+    insertAtPos(head,tail,4,20);
+    print(head);
+
+    cout<<"head "<<head->data<<endl;
+    cout<<"tail "<<tail->data<<endl;
     return 0;
 }
