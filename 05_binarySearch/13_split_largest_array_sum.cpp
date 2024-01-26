3675ee1a7b700a957717cd94d00e1d4eac9cc723
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-26 21:55:49 +0545

    added leetcode problem

---
+++ 05_binarySearch/13_split_largest_array_sum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int splitArray(vector<int>& nums, int k) {
+    int start=0,end=0;
+    for(int i:nums){
+        start=max(start,i);
+        end+=i;
+    }
+    int mid = start+(end-start)/2;
+    while(start<end){
+        int pieces=1;
+        int sum=0;
+        for(int i:nums){
+            if(sum+i>mid){
+                sum=i;
+                pieces++;
+            }
+            else{
+                sum+=i;
+            }
+            if(pieces>k){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+            mid=start+(end-start)/2;
+        }
+    }
+    return end;
+}
+
+int main(){
+    return 0;
+}
