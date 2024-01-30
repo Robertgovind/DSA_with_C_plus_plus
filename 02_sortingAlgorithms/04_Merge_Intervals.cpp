876df98f880103fc39f1711280b5673444010889
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-30 21:06:40 +0545

    leetcode problem

--- 02_sortingAlgorithms/04_Merge_Intervals.cpp
+++ 02_sortingAlgorithms/04_Merge_Intervals.cpp
@@ -9,13 +9,14 @@ vector<vector<int>> merge(vector<vector<int>>& intervals) {
         output = intervals;
         return output;
     }
-    
+    sort(intervals.begin(),intervals.end());
     int first=intervals[0][0];
     int second=intervals[0][1];
     temp[0]=first;
     for(int i=1;i<intervals.size();i++){
         if(intervals[i][0]<=second){
-            second = intervals[i][1];
+            temp[0]=min(first,intervals[i][0]);
+            second = max(second,intervals[i][1]);
         }
         else{
             temp[1]=second;
@@ -29,8 +30,4 @@ vector<vector<int>> merge(vector<vector<int>>& intervals) {
     temp[1]=second;
     output.push_back(temp);
     return output;
-}
-
-int main(){
-    return 0;
 }
