0bf2c16d3be8f0bb3ec0bd4c5dc67c6f786dd0c5
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-02-05 21:51:58 +0545

    dp problem

---
+++ DP/02_climbing_stairs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int climbStairs(int n){
+    int arr[n+1];
+    arr[0] = 1;
+    arr[1]=1;
+    for(int i =2;i<=n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+}
+
+int main(){
+    cout<<climbStairs(4)<<endl;
+}
