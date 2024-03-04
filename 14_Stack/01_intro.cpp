6035c494e6036d613c60c19d33aad276c3592246
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-03-05 00:57:01 +0545

    modified

--- 14_Stack/01_intro.cpp
+++ 14_Stack/01_intro.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
+    stack<int> s;
+    s.push(3);
+    s.push(5);
+    cout<<"Top "<< s.top() << endl;
+    s.pop();
+    cout<<"Top "<<s.top()<< endl;
+    cout<<s.empty()<<endl;
     return 0;
 }
