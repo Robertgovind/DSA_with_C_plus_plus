8f94de45f444da0ae3997b1da9fd7cf6e0cffcc3
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-26 17:12:16 +0545

    creating tree in level order traversal

---
+++ 16_Binary_tree/03_tree_creation.cpp
@@ -0,0 +1,62 @@
+// Creation of a tree by taking inpurs in level ordering
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void TreeFromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    cout << "Enter the data for root : " << endl;
+    int data;
+    cin >> data;
+
+    root = new node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        cout << "Enter data for left of " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1)
+        {
+            temp->left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter data for right of " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1)
+        {
+            temp->right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+    return 0;
+}
