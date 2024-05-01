c44121924cb598f909d54e3c9ade1d209eb83452
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-05-01 18:55:00 +0545

    finding kth ancestor in binary tree

---
+++ 16_Binary_tree/21_Kth_ancestor.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *solve(Node *root, int &k, int node)
+{
+    if (root == NULL)
+        return NULL;
+    if (root->data == node)
+        return root;
+    Node *leftAns = solve(root->left, k, node);
+    Node *rightAns = solve(root->right, k, node);
+
+    if (leftAns != NULL && rightAns == NULL)
+    {
+        k--;
+        if (k <= 0)
+        {
+            k = INT8_MAX;
+            return root;
+        }
+        return leftAns;
+    }
+    if (leftAns == NULL && rightAns != NULL)
+    {
+        k--;
+        if (k <= 0)
+        {
+            k = INT8_MAX;
+            return root;
+        }
+        return rightAns;
+    }
+    return NULL;
+}
+
+int KthAncestor(Node *root, int k, int node)
+{
+    Node *ans = solve(root, k, node);
+    if (ans == NULL || ans->data == node)
+        return -1;
+
+    return ans->data;
+}
