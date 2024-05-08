f3d26d9fcc5f8755282671976bcc83155f11cde3
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-05-08 19:18:27 +0545

    find kth smallest ancestor

---
+++ 16_Binary_tree/32_kth_smallest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+};
+
+int solve(Node *root, int &i, int k)
+{
+    if (root == NULL)
+        return -1;
+
+    int left = solve(root->left, i, k);
+    if (left != -1)
+        return left;
+
+    i++;
+    if (i == k)
+        return root->data;
+
+    return solve(root->right, i, k);
+}
+
+int kthSmallest(Node *root, int k)
+{
+    int i = 0;
+    int ans = solve(root, i, k);
+    return ans;
+}
