2372df33863731bc629b0d857de7c85522cc928c
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-06-03 22:42:17 +0545

    soduko solver

---
+++ 11_Backtracking/03_soduko_solver.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(int row, int col, vector<vector<int>> &board, int val)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        // for row
+        if (board[row][i] == val)
+        {
+            return false;
+        }
+        // for column
+        if (board[i][col] == val)
+        {
+            return false;
+        }
+        // for 3x3 matrix
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i / 3] == val)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool solve(vector<vector<int>> &borad)
+{
+    int n = board[0].size();
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+
+            if (board[row][col] == 0)
+            {
+                for (int val = 1; val <= 9; val++)
+                {
+                    if (isSafe(row, col, board, val))
+                    {
+                        board[row][col] = val;
+                        bool nextSoln = solve(borad);
+                        if (nextSoln)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            board[row][col] = 0;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
